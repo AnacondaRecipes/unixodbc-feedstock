116f8cc4c3cdb56166fe9723f8fae2029b017c06
Michael Sarahan <msarahan@gmail.com>
2018-04-03 17:31:17 -0500

    update to 2.3.6

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,5 +6,6 @@ set -x
 ./configure --prefix=${PREFIX}  \
             --build=${BUILD} \
             --sysconfdir=/etc
+git apply $RECIPE_DIR/0001-fix-sed-syntax-on-Mac.patch
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
