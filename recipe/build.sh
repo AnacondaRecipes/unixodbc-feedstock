03dade7a5cb364f105f64685eb357d08bb7e2da5
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-08-24 05:24:37 -0500

    Add support for libedit

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,11 @@
 set -e
 set -x
 
+autoreconf -vfi
+
 ./configure --prefix=${PREFIX}  \
             --build=${BUILD} \
+            --enable-editline=yes \
             --sysconfdir=/etc
-git apply $RECIPE_DIR/0001-fix-sed-syntax-on-Mac.patch
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
