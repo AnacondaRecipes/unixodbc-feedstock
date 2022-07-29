cac2edab9599fffb971f12ba9a36bd2d816ef5e0
Udo-Peter Steyer <ous8292@gmail.com>
2022-07-29 14:57:23 -0500

    fixing CLOCK_REALTIME issue

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,15 @@
 set -e
 set -x
 
+export CFLAGS="-O2 ${CFLAGS}"
+export CXXFLAGS="-O2 ${CXXFLAGS}"
+
 autoreconf -vfi
 
 ./configure --prefix=${PREFIX}  \
             --build=${BUILD} \
             --enable-editline=yes \
             --sysconfdir=/etc
+
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
